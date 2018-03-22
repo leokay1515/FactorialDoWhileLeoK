46b7404334cb0ebe8b6dce9f3988dc783fe5e3bd
leokay1515 <36515738+leokay1515@users.noreply.github.com>
2018-03-22 11:39:30 -0400

    Initial commit

---
+++ FactorialDoWhileLeoK/FactorialDoWhileLeoKForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace FactorialDoWhileLeoK
+{
+    partial class frmFactorialDoWhileLeoK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRequest = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.lstNumbers = new System.Windows.Forms.ListBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblRequest
+            // 
+            this.lblRequest.AutoSize = true;
+            this.lblRequest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRequest.Location = new System.Drawing.Point(12, 30);
+            this.lblRequest.Name = "lblRequest";
+            this.lblRequest.Size = new System.Drawing.Size(195, 20);
+            this.lblRequest.TabIndex = 0;
+            this.lblRequest.Text = "Please enter a number:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(215, 30);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // lstNumbers
+            // 
+            this.lstNumbers.FormattingEnabled = true;
+            this.lstNumbers.Location = new System.Drawing.Point(96, 110);
+            this.lstNumbers.Name = "lstNumbers";
+            this.lstNumbers.Size = new System.Drawing.Size(120, 95);
+            this.lstNumbers.TabIndex = 2;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(57, 221);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 20);
+            this.lblAnswer.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(109, 66);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(98, 28);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmFactorialDoWhileLeoK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 261);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lstNumbers);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblRequest);
+            this.Name = "frmFactorialDoWhileLeoK";
+            this.Text = "Factorial Do While by Leo Kay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRequest;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.ListBox lstNumbers;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ FactorialDoWhileLeoK/FactorialDoWhileLeoKForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactorialDoWhileLeoK
+{
+    public partial class frmFactorialDoWhileLeoK : Form
+    {
+        public frmFactorialDoWhileLeoK()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Local variables
+            Double facAns;
+            Double facNum;
+            int facCounter;
+
+            //Clear the list box
+            this.lstNumbers.Items.Clear();
+
+            //Initialize the final answer to 1
+            facAns = 1;
+
+            //Set the users input to facNum
+            facNum = Convert.ToDouble(this.txtNumber.Text);
+
+            //Set the counter to 0
+            facCounter = 0;
+
+            //loop until the counter is equal to the facNum
+            do
+            {
+                //Add 1 to the counter
+                facCounter = facCounter + 1;
+
+                //Add the counter number to the list
+                lstNumbers.Items.Add(facCounter);
+
+                //Multiply the counter by the answer
+                facAns = facAns * facCounter;
+            } while (facNum >= facCounter + 1);
+
+            //Display the answer 
+            this.lblAnswer.Text = this.txtNumber.Text + "! = " + Convert.ToString(facAns);
+        }
+    }
+}
